2402f6a8390b2c12db10775f7ab0c190b1a93675
Ilia B <festeloqq@gmail.com>
2024-01-17 21:23:09 +0100

    T5, T6 - implement log in

--- lib/features/auth/login/login_presentation_model.dart
+++ lib/features/auth/login/login_presentation_model.dart
@@ -1,3 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_demo/core/domain/model/user.dart';
+import 'package:flutter_demo/core/utils/bloc_extensions.dart';
+import 'package:flutter_demo/features/auth/domain/model/log_in_failure.dart';
 import 'package:flutter_demo/features/auth/login/login_initial_params.dart';
 
 /// Model used by presenter, contains fields that are relevant to presenters and implements ViewModel to expose data to view (page)
@@ -7,17 +11,24 @@ class LoginPresentationModel implements LoginViewModel {
     // ignore: avoid_unused_constructor_parameters
     LoginInitialParams initialParams,
   )   : username = '',
-        password = '';
+        password = '',
+        logInResult = const FutureResult.empty();
 
   /// Used for the copyWith method
   LoginPresentationModel._({
     required this.username,
     required this.password,
+    required this.logInResult,
   });
 
   final String username;
 
   final String password;
+
+  final FutureResult<Either<LogInFailure, User>> logInResult;
+
+  @override
+  bool get isLoading => logInResult.isPending();
 
   @override
   bool get isLoginEnabled => username.isNotEmpty && password.isNotEmpty;
@@ -25,15 +36,18 @@ class LoginPresentationModel implements LoginViewModel {
   LoginPresentationModel copyWith({
     String? username,
     String? password,
+    FutureResult<Either<LogInFailure, User>>? logInResult,
   }) {
     return LoginPresentationModel._(
       username: username ?? this.username,
       password: password ?? this.password,
+      logInResult: logInResult ?? this.logInResult,
     );
   }
 }
 
 /// Interface to expose fields used by the view (page).
 abstract class LoginViewModel {
+  bool get isLoading;
   bool get isLoginEnabled;
 }
--- lib/features/auth/login/login_presenter.dart
+++ lib/features/auth/login/login_presenter.dart
@@ -1,4 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_demo/core/utils/bloc_extensions.dart';
+import 'package:flutter_demo/features/auth/domain/use_cases/log_in_use_case.dart';
 import 'package:flutter_demo/features/auth/login/login_navigator.dart';
 import 'package:flutter_demo/features/auth/login/login_presentation_model.dart';
 
@@ -6,9 +8,11 @@ class LoginPresenter extends Cubit<LoginViewModel> {
   LoginPresenter(
     LoginPresentationModel super.model,
     this.navigator,
+    this.logInUseCase,
   );
 
   final LoginNavigator navigator;
+  final LogInUseCase logInUseCase;
 
   // ignore: unused_element
   LoginPresentationModel get _model => state as LoginPresentationModel;
@@ -19,5 +23,14 @@ class LoginPresenter extends Cubit<LoginViewModel> {
 
   void onPasswordChanged(String value) {
     emit(_model.copyWith(password: value));
+  }
+
+  Future<void> onLoginPressed() async {
+    await logInUseCase
+        .execute(
+          username: _model.username,
+          password: _model.password,
+        )
+        .observeStatusChanges((result) => emit(_model.copyWith(logInResult: result)));
   }
 }
