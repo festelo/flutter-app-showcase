67db02c21804ec9d52f34ff200ff72673ccaad8e
Ilia B <festeloqq@gmail.com>
2024-01-17 21:23:09 +0100

    T7 & T8 - handle log in use case result

--- lib/features/auth/login/login_presenter.dart
+++ lib/features/auth/login/login_presenter.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter_demo/core/utils/bloc_extensions.dart';
+import 'package:flutter_demo/core/utils/either_extensions.dart';
 import 'package:flutter_demo/features/auth/domain/use_cases/log_in_use_case.dart';
 import 'package:flutter_demo/features/auth/login/login_navigator.dart';
 import 'package:flutter_demo/features/auth/login/login_presentation_model.dart';
@@ -31,6 +32,10 @@ class LoginPresenter extends Cubit<LoginViewModel> {
           username: _model.username,
           password: _model.password,
         )
-        .observeStatusChanges((result) => emit(_model.copyWith(logInResult: result)));
+        .observeStatusChanges((result) => emit(_model.copyWith(logInResult: result)))
+        .asyncFold(
+          (fail) => navigator.showError(fail.displayableFailure()),
+          (success) => navigator.showAlert(title: 'Have I passed?', message: 'Hope so :)'),
+        );
   }
 }
